9d88abe7b31710c54487ea3d2de11e9833efe75d
SyedMaazHassan <hafizmaazhassan33@gmail.com>
2022-03-11 01:16:46 +0500

    POSTgres ADDed

--- webapp/settings.py
+++ webapp/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd7rpl4qgfa4486',
+        'USER': 'gqaufcdolcsykk',
+        'PASSWORD': 'gqaufcdolcsykk',
+        'HOST': 'ec2-3-216-221-31.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
